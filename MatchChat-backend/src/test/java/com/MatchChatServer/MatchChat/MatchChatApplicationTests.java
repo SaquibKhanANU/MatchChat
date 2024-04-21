3f26dd3120a237c262eac2d4a86959fad3f82bcb
Saquib Khan <u7108513@anu.edu.au>
2024-04-21 11:54:48 +1000

    initial commit

---
+++ MatchChat-backend/src/test/java/com/MatchChatServer/MatchChat/MatchChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatchChatServer.MatchChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
