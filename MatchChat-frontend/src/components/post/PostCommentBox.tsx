1fe6d2a3ab9dedd3612212733a46e4d7f30d27dc
U7108513 <u7108513@anu.edu.au>
2024-04-23 15:08:59 +1000

    Comment box started

--- MatchChat-frontend/src/components/post/PostCommentBox.tsx
+++ MatchChat-frontend/src/components/post/PostCommentBox.tsx
@@ -1,9 +1,20 @@
-
+import "./PostCommentBox.css";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function PostCommentBox(): JSX.Element {
+  const [value, setValue] = useState("");
+
   return (
-    <div>
-      CommentBox
+    <div className="post-comment-box-container">
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        placeholder="Add a comment"
+      />
+      <button className="post-comment-box-button">Comment</button>
     </div>
   );
 }
