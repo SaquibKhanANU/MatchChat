d01c33196a99935d9e7d1fe80db8afaf4a64127f
U7108513 <u7108513@anu.edu.au>
2024-04-24 13:35:25 +1000

    services created

---
+++ MatchChat-backend/user-auth-service/src/test/java/com/MatchChat/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatchChat.userauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
