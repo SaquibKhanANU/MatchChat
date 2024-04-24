80b34ffec8236a1dbb047e329b6b4cb94e21f738
U7108513 <u7108513@anu.edu.au>
2024-04-24 12:59:16 +1000

    match post service

---
+++ MatchChat-backend/match-post-service/src/main/java/com/MatchChat/matchpostservice/MatchPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.MatchChat.matchpostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchPostServiceApplication.class, args);
+	}
+
+}
---
+++ MatchChat-backend/match-post-service/src/test/java/com/MatchChat/matchpostservice/MatchPostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatchChat.matchpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchPostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
